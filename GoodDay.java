4fb5f97cc73fe8e309b09e6af016dfbed9843319
Bea Sanssi <bs222eh@student.lnu.se>
2024-09-05 16:18:15 +0200

    Start up project

---
+++ GoodDay.java
@@ -0,0 +1,15 @@
+/**
+ * This represents the class for the HelloThere task.
+ */
+public class GoodDay {
+
+  /**
+   * This represents the class for greeting the user.
+   *
+   * @param name - The name of the person to greet
+   * @return - The greeting
+   */
+  public String goodDay(String name) {
+    return "Good Day, " + name + ". I hope you have a great day!";
+  }   
+}
